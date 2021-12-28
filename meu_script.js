6d11b51727ad43681990768b89abb149b4711518
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2021-12-28 02:36:12 -0300

    Uses contact reference to send message.

--- meu_script.js
+++ meu_script.js
@@ -12,19 +12,27 @@
 //   console.log("fim: escolher contato")
 // }, 15000);//wait 15 seconds
 
-setTimeout(function(){
-  console.log("inicio: send")
-  var send = document.querySelector('[data-icon="send"]');
-  // var send = document.querySelector('button._3HQNh._1Ae7k');
-  console.log(send)
+var contact = 'Side'
 
-  function simulateMouseEvents(element, eventName) {
+function simulateMouseEvents(element, eventName) {
   var mouseEvent = document.createEvent ('MouseEvents');
   mouseEvent.initEvent (eventName, true, true);
   element.dispatchEvent (mouseEvent);
 }
 
-  simulateMouseEvents(send, 'mousedown')
-  send.click();
-  console.log("fim: send");
-}, 15000);//wait 15 seconds
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+setTimeout(function(){
+  console.log("inicio: send")
+
+  var contact_in_list = document.querySelector(`[title="${contact}"]`).parentNode.parentNode.parentNode.parentNode
+  simulateMouseEvents(contact_in_list, 'mousedown');
+
+  setTimeout(function(){
+    var send = document.querySelector('[data-icon="send"]');
+    send.click();
+    console.log("fim: send");
+  }, 1000);//wait 1 second
+}, 20000);//wait 20 seconds
