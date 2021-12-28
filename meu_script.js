b8b898fbd3cd1f9979e53a2c84bb32d70a4f33fd
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2021-12-28 04:05:39 -0300

    Differentiate rigorously between contacts

--- meu_script.js
+++ meu_script.js
@@ -1,18 +1,33 @@
-// setTimeout(function(){
-//   console.log("inicio: escolher")
-//   var my_number = document.querySelector('[title="+55 11 94230-1911"]').closest('._3OvU8');
-//
-//   function simulateMouseEvents(element, eventName) {
-//   var mouseEvent= document.createEvent ('MouseEvents');
-//   mouseEvent.initEvent (eventName, true, true);
-//   element.dispatchEvent (mouseEvent);
-// }
-//
-//   simulateMouseEvents(my_number, 'mousedown')
-//   console.log("fim: escolher contato")
-// }, 15000);//wait 15 seconds
+function define_contact_location(){
+  var all_contact_occurrences = document.querySelectorAll(`[title="${contact}"]`)
+  var contact_in_list, contact_in_chat
 
-var contact = 'Side'
+  all_contact_occurrences.forEach(function(contact_occurrence){
+      if (contact_occurrence.parentNode.tagName == 'SPAN') {
+        contact_in_list = contact_occurrence.parentNode.parentNode.parentNode.parentNode;
+      } else if (contact_occurrence.parentNode.tagName == 'DIV') {
+        contact_in_chat = contact_occurrence.parentNode.parentNode.parentNode;
+      }
+    })
+    return {
+        contact_in_list,
+        contact_in_chat
+    };
+}
+
+function send_message() {
+  setTimeout(function(){
+    console.log("inicio: send")
+    let {contact_in_list, contact_in_chat} = define_contact_location()
+    simulateMouseEvents(contact_in_list, 'mousedown');
+
+    setTimeout(function(){
+      var send = document.querySelector('[data-icon="send"]');
+      send.click();
+      console.log("fim: send");
+    }, 1000);//wait 1 second
+  }, 20000);//wait 20 seconds
+}
 
 function simulateMouseEvents(element, eventName) {
   var mouseEvent = document.createEvent ('MouseEvents');
@@ -20,19 +35,5 @@ function simulateMouseEvents(element, eventName) {
   element.dispatchEvent (mouseEvent);
 }
 
-function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
-setTimeout(function(){
-  console.log("inicio: send")
-
-  var contact_in_list = document.querySelector(`[title="${contact}"]`).parentNode.parentNode.parentNode.parentNode
-  simulateMouseEvents(contact_in_list, 'mousedown');
-
-  setTimeout(function(){
-    var send = document.querySelector('[data-icon="send"]');
-    send.click();
-    console.log("fim: send");
-  }, 1000);//wait 1 second
-}, 20000);//wait 20 seconds
+var contact = 'Side'
+send_message()
