72513f7912fbbed0af687deb95dde408c9497a74
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2021-12-29 03:20:18 -0300

    Simple functions to trigger message to be sent and unload trigger

--- meu_script.js
+++ meu_script.js
@@ -1,79 +1,92 @@
-function get_contact_locations(contact){
-  var all_contact_occurrences = document.querySelectorAll(`[title="${contact}"]`)
-  var contact_in_list, contact_in_chat
+// function get_contact_locations(contact){
+//   var all_contact_occurrences = document.querySelectorAll(`[title="${contact}"]`)
+//   var contact_in_list, contact_in_chat
+//
+//   all_contact_occurrences.forEach(function(contact_occurrence){
+//       if (contact_occurrence.parentNode.tagName == 'SPAN') {
+//         contact_in_list = contact_occurrence.parentNode.parentNode.parentNode.parentNode;
+//       } else if (contact_occurrence.parentNode.tagName == 'DIV') {
+//         contact_in_chat = contact_occurrence.parentNode.parentNode.parentNode;
+//       }
+//     })
+//     return {
+//         contact_in_list,
+//         contact_in_chat
+//     };
+// }
+//
+// function simulateMouseEvents(element, eventName) {
+//   var mouseEvent = document.createEvent ('MouseEvents');
+//   mouseEvent.initEvent (eventName, true, true);
+//   element.dispatchEvent (mouseEvent);
+// }
+//
+// function clickOnContact(contact){
+//   let {contact_in_list, contact_in_chat} = get_contact_locations(contact);
+//   simulateMouseEvents(contact_in_list, 'mousedown');
+// }
+//
+// function getStatus(contact) {
+//   let {contact_in_list, contact_in_chat} = get_contact_locations(contact);
+//   let statusElement = contact_in_chat.querySelector('[class$="selectable-text copyable-text"]');
+//   return statusElement
+// }
 
-  all_contact_occurrences.forEach(function(contact_occurrence){
-      if (contact_occurrence.parentNode.tagName == 'SPAN') {
-        contact_in_list = contact_occurrence.parentNode.parentNode.parentNode.parentNode;
-      } else if (contact_occurrence.parentNode.tagName == 'DIV') {
-        contact_in_chat = contact_occurrence.parentNode.parentNode.parentNode;
-      }
-    })
-    return {
-        contact_in_list,
-        contact_in_chat
-    };
-}
 
-function simulateMouseEvents(element, eventName) {
-  var mouseEvent = document.createEvent ('MouseEvents');
-  mouseEvent.initEvent (eventName, true, true);
-  element.dispatchEvent (mouseEvent);
-}
+// function waitToSend(contact){
+//   var status
+//   status = getStatus(contact);
+//   status.addEventListener('DOMSubtreeModified', a=>{
+//     setTimeout(function(){
+//       if (status.title == 'online' || status.title.substr(-3) == '...') {
+//         send_message();
+//       }
+//     }, 1000)
+//   })
+// }
 
-function clickOnContact(contact){
-  let {contact_in_list, contact_in_chat} = get_contact_locations(contact);
-  simulateMouseEvents(contact_in_list, 'mousedown');
-}
-
-function getStatus(contact) {
-  let {contact_in_list, contact_in_chat} = get_contact_locations(contact);
-  let statusElement = contact_in_chat.querySelector('[class$="selectable-text copyable-text"]');
-  var status
-  if (statusElement == null) {
-    status = 'undefined'
-  } else if (statusElement.title == 'online') {
-    status = 'online'
-  } else {
-    status = 'offline'
-  }
-  return status
-}
-
-function send_message() {
+function sendmessage(){
   var send = document.querySelector('[data-icon="send"]');
+  if (send == null) {console.log("There's no message to send"); return}
   send.click();
 }
 
-setTimeout(function(){
-  var contact = 'Side';
-  clickOnContact(contact)
+function sendMessageWhenOnline(){
   setTimeout(function(){
-    var status = getStatus(contact)
-function getStatusCurrent(){
-  var allPossibleChatContacts = document.querySelector('div[role="button"] > div > div > span[title][dir]:not([class$=" selectable-text copyable-text"])').parentElement.parentElement.parentElement.querySelector('span[class$=" selectable-text copyable-text"]')
-  return allPossibleChatContacts
+    var statusLocation = getStatusLocationCurrent()
+    var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]')
+    if (status != null){
+      if (status.title == 'online' || status.title.substr(-3) == '...') {
+        sendmessage()
+        removeWait()
+      }
+    }
+  }, 1000)
+}
+
+function getStatusLocationCurrent(){
+  var statusLocation = document.querySelector('div[role="button"] > div > div > span[title][dir]:not([class$=" selectable-text copyable-text"])').parentElement.parentElement.parentElement
+  // .querySelector('span[class$=" selectable-text copyable-text"]')
+  // console.log(statusLocation)
+  return statusLocation
 }
 
 function waitToSendCurrent(){
-  var status = getStatusCurrent()
-  if (status.title == 'online'){
-    send_message();
-  } else {
-    status.addEventListener('DOMSubtreeModified', a=>{
-      setTimeout(function(){
-        if (status.title == 'online' || status.title.substr(-3) == '...') {
-          send_message();
-        }
-      }, 1000)
-    })
+  console.log(1)
+  var statusLocation = getStatusLocationCurrent()
+  console.log(2)
+  var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]')
+  console.log(3)
+  if (status != null) {
+    if (status.title == 'online'){
+      sendmessage()
+      return
+    }
   }
+  statusLocation.addEventListener('DOMSubtreeModified', sendMessageWhenOnline)
 }
 
-    if (status == 'online') {
-      send_message()
-    } else {
-      console.log('Ainda não está online')
-    }
-  }, 2000);//wait 2 seconds
-}, 20000);//wait 20 seconds
+function removeWait(){
+  var statusLocation = getStatusLocationCurrent()
+  statusLocation.removeEventListener('DOMSubtreeModified', sendMessageWhenOnline)
+}
