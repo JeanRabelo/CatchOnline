911c41b5940c04506ebda81723cde61851f24f49
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2022-01-01 10:22:31 -0300

    working with manifest 3

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-chrome.browserAction.onClicked.addListener(buttonClicked);
+console.log(1);
+
+chrome.action.onClicked.addListener(tab => {buttonClicked(tab)});
 
 function buttonClicked(tab){
+  console.log(2);
   let msg = {
     txt: 'hello'
   }
   chrome.tabs.sendMessage(tab.id, msg);
+  console.log(3);
 }
--- content_script.js
+++ content_script.js
@@ -1,24 +1,37 @@
+console.log(4)
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse){handleTriggerButton();}
 
 function sendmessage(){
+  console.log(16)
   var send = document.querySelector('[data-icon="send"]');
+  console.log("send = ")
+  console.log(send)
   if (send == null) {console.log("There's no message to send"); return}
   send.click();
 }
 
 function sendMessageWhenOnline(){
-  setTimeout(function(){
-    var statusLocation = getStatusLocationCurrent();
-    var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]')
-    if (status != null){
+  console.log(12)
+  var statusLocation = getStatusLocationCurrent();
+  var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]')
+  console.log(13)
+  if (status != null){
+    console.log(14)
+    console.log("status =")
+    console.log(status) //só tá funcionando até aqui!!!
+    console.log('status.title =')
+    setTimeout(function (){
+    console.log(status.title)
       if (status.title == 'online' || status.title.substr(-3) == '...') {
+        console.log(15)
         sendmessage();
         removeWait(statusLocation);
       }
-    }
-  }, 1000)
+    }, 500)
+
+  }
 }
 
 
@@ -28,29 +41,34 @@ function getStatusLocationCurrent(){
 }
 
 function waitToSendCurrent(statusLocation){
+  console.log(8)
   var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]');
   if (status != null) {
+    console.log(9)
     if (status.title == 'online'){
+      console.log(10)
       sendmessage();
       return
     }
   }
   if (!statusLocation.hasAttribute('beingListened')){
-    statusLocation.addEventListener('DOMSubtreeModified', sendMessageWhenOnline)
+    console.log(11)
     var attBeingListened = document.createAttribute('beingListened'); //room for improvement
     statusLocation.setAttributeNode(attBeingListened); //room for improvement
+    statusLocation.addEventListener('DOMSubtreeModified', sendMessageWhenOnline)
   }
 }
 
 function handleTriggerButton(){
-  setTimeout(function(){
-    var statusLocation = getStatusLocationCurrent();
-    if (!statusLocation.hasAttribute('beingListened')){
-      waitToSendCurrent(statusLocation)
-      return
-    }
-    removeWait(statusLocation)
-  }, 1000)
+  console.log(5)
+  var statusLocation = getStatusLocationCurrent();
+  if (!statusLocation.hasAttribute('beingListened')){
+    console.log(6)
+    waitToSendCurrent(statusLocation)
+    return
+  }
+  console.log(7)
+  removeWait(statusLocation)
 }
 
 function removeWait(statusLocation){
