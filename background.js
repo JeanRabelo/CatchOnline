a90cc8bf26358f23690be84702cb17e8e077a4cc
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2022-01-01 11:19:44 -0300

    Refactor

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-console.log(1);
-
 chrome.action.onClicked.addListener(tab => {buttonClicked(tab)});
 
 function buttonClicked(tab){
-  console.log(2);
   let msg = {
     txt: 'hello'
   }
   chrome.tabs.sendMessage(tab.id, msg);
-  console.log(3);
 }
--- content_script.js
+++ content_script.js
@@ -1,39 +1,26 @@
-console.log(4)
-chrome.runtime.onMessage.addListener(gotMessage);
+chrome.runtime.onMessage.addListener(handleTriggerButton);
 
-function gotMessage(message, sender, sendResponse){handleTriggerButton();}
+// function gotMessage(message, sender, sendResponse){();}
 
 function sendmessage(){
-  console.log(16)
   var send = document.querySelector('[data-icon="send"]');
-  console.log("send = ")
-  console.log(send)
   if (send == null) {console.log("There's no message to send"); return}
   send.click();
 }
 
 function sendMessageWhenOnline(){
-  console.log(12)
   var statusLocation = getStatusLocationCurrent();
   var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]')
-  console.log(13)
+
   if (status != null){
-    console.log(14)
-    console.log("status =")
-    console.log(status) //só tá funcionando até aqui!!!
-    console.log('status.title =')
     setTimeout(function (){
-    console.log(status.title)
       if (status.title == 'online' || status.title.substr(-3) == '...') {
-        console.log(15)
         sendmessage();
         removeWait(statusLocation);
       }
     }, 500)
-
   }
 }
-
 
 function getStatusLocationCurrent(){
   var statusLocation = document.querySelector('div[role="button"] > div > div > span[title][dir]:not([class$=" selectable-text copyable-text"])').parentElement.parentElement.parentElement;
@@ -41,18 +28,14 @@ function getStatusLocationCurrent(){
 }
 
 function waitToSendCurrent(statusLocation){
-  console.log(8)
   var status = statusLocation.querySelector('span[class$=" selectable-text copyable-text"]');
   if (status != null) {
-    console.log(9)
     if (status.title == 'online'){
-      console.log(10)
       sendmessage();
       return
     }
   }
   if (!statusLocation.hasAttribute('beingListened')){
-    console.log(11)
     var attBeingListened = document.createAttribute('beingListened'); //room for improvement
     statusLocation.setAttributeNode(attBeingListened); //room for improvement
     statusLocation.addEventListener('DOMSubtreeModified', sendMessageWhenOnline)
@@ -60,14 +43,14 @@ function waitToSendCurrent(statusLocation){
 }
 
 function handleTriggerButton(){
-  console.log(5)
+
   var statusLocation = getStatusLocationCurrent();
   if (!statusLocation.hasAttribute('beingListened')){
-    console.log(6)
-    waitToSendCurrent(statusLocation)
+
+    waitToSendCurrent(statusLocation);
     return
   }
-  console.log(7)
+
   removeWait(statusLocation)
 }
 
